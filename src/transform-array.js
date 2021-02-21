c93dd3d5432227ed57a92acf21e9b6fce25e42bf
lizafirstwin <66125093+lizafirstwin@users.noreply.github.com>
2021-02-21 18:19:54 +0300

    refactor: apply transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr)
+{
+  if(!Array.isArray(arr)) throw Error;
+	let arr2 = [];
+  for(let i = 0; i < arr.length; i++){		  	
+   switch(arr[i]){
+   	case  '--double-next': 
+    	if(i == arr.length - 1) return arr2;
+			else arr2.push(arr[i + 1]);
+      break;
+    case '--double-prev': 
+    	if(i == 0) ;
+      else arr2.push(arr[i - 1])
+      break;
+    case '--discard-prev': 
+    	arr2.pop(arr[i]);
+      break
+    case '--discard-next':
+      if(i == arr.length - 1) return arr2;
+      if(arr[i + 2] == '--double-prev' || arr[i + 2] == '--discard-prev') i += 2;
+    	else i++;
+    	break;
+    default: arr2.push(arr[i]);
+		}
+  }
+	return arr2;
 };
