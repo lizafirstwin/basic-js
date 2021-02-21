9fc1db79aa62f84ad8cadb58422bfed87efab3e5
lizafirstwin <66125093+lizafirstwin@users.noreply.github.com>
2021-02-21 18:18:03 +0300

    refactor: apply dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+    
+if(!Array.isArray(arr)) return false;
+let arr2 = [];
+for(let i = 0; i < arr.length; i++){
+	if(typeof(arr[i]) === 'string'){
+  arr[i] = arr[i].trim();
+  	arr2.push(arr[i][0].toUpperCase());
+  }
+}
+arr = arr2.sort().join('');
+return arr;
 };
