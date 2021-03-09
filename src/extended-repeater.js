42f06ff318d87ff6b0429ff6ac9ebe988f64cf4a
lizafirstwin <66125093+lizafirstwin@users.noreply.github.com>
2021-03-09 16:27:51 +0300

    refactor: add extended_repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	let flag = 0;
+  let arr = [];
+  let newStr;
+  if(!options.repeatTimes && !options.additionRepeatTimes) return str + options.addition;
+  while(flag < options.additionRepeatTimes) {
+  	str = str + options.addition;
+    flag++;
+  }
+  flag = 0;
+  while (flag < options.repeatTimes ) {
+  	arr.push(str);
+  	flag++;
+    if(options.separator)  newStr = arr.join(options.separator);
+    else newStr = arr.join('+')
+  }
+  return newStr;
 };
-  
