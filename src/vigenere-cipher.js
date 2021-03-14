7251c761bd82703fd268dc3a3d1a1ab22dc3124b
lizafirstwin <66125093+lizafirstwin@users.noreply.github.com>
2021-03-14 23:17:34 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(word, key) {
+    if(word.length > key.length) {
+      key += key;
+    }
+      let alphabet = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z'.split(', ').join('');
+      
+      let chyper = [];
+      for(let i = 0; i < word.length; i++) {
+        chyper[i]  = alphabet[(alphabet.indexOf(word[i]) + alphabet.indexOf(key[i])) % 26];
+      }
+      return chyper.join('').toUpperCase();
+    }
+  decrypt(chyper, key) {
+    let word = [];
+    let alphabet = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z'.split(', ').join('');
+    for(let i = 0; i < chyper.length; i++) {
+      word[i]  = alphabet[(alphabet.indexOf(chyper[i]) - alphabet.indexOf(key[i]) + 26) % 26];
+    }
+    return word.join('').toUpperCase();
   }
 }
 
