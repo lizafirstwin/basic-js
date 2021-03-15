2ddac8fc2adeb889b19e8450ecfbe6085cd56da7
lizafirstwin <66125093+lizafirstwin@users.noreply.github.com>
2021-03-15 12:43:12 +0300

    fix: recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(array, depth = 1) {
-    return arr.find((item) => Array.isArray(item))
+    return array.find((item) => Array.isArray(item))
       ? this.calculateDepth(array.flat(1), depth + 1) : depth;
   }
 };
